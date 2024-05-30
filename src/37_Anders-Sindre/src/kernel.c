ebee14192dce510f90197c8ceee7ee811a3eedce
Anders Liavåg Martinsen <anderslm@uia.no>
2024-05-30 11:13:28 +0200

    added assignment 3

--- src/37_Anders-Sindre/src/kernel.c
+++ src/37_Anders-Sindre/src/kernel.c
@@ -4,8 +4,10 @@
 #include "libc/string.h"
 #include "libc/stdio.h"
 #include "gdt.h"
+#include "idt.h"
 #include <multiboot2.h>
-
+#include "var.h"
+#include "interrupts.h"
 
 
 struct multiboot_info {
@@ -19,12 +21,12 @@ int kernel_main();
 
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
     init_gdt();
-
+    init_idt();
     char* hello_world = "Hello, World1!\nHello, World2!";
     size_t len = strlen(hello_world);
 
     print(hello_world, len);
-
+    init_irq();
 
     // Call cpp kernel_main (defined in kernel.cpp)
     return kernel_main();
--- src/37_Anders-Sindre/src/kernel.cpp
+++ src/37_Anders-Sindre/src/kernel.cpp
@@ -1,6 +1,24 @@
-
+extern "C"{
+    #include "var.h"
+    #include "interrupts.h"
+    #include "ascii_table.h"
+    #include "libc/stddef.h"
+}
 
 extern "C" int kernel_main(void);
 int kernel_main(){
+// Register IRQ handler for keyboard (IRQ1)
+    register_irq_handler(IRQ1, [](registers_t*, void*) {
+        // Read from keyboard
+        unsigned char scan_code = inb(0x60);
+        char f = scancode_to_ascii(&scan_code);
+
+        //printf("%c", f);
+        
+
+        // Disable
+        asm volatile("cli");
+    }, NULL);
+
     return 0;
 }
